150e3569b34bc1e849774b97497a37261c5727df
chaewon <cwlee4633@naver.com>
2023-11-27 13:55:00 +0900

    제네릭 타입의 Stack 구현과 괄호의 짝검사

---
+++ src/main/java/com/example/codingtest/stack_queue/StackPrac.java
@@ -0,0 +1,23 @@
+package com.example.codingtest.stack_queue;
+
+//괄호의 짝 검사
+public class StackPrac {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
